b9b757a110eb5e7fd8e2024dc5c7a918383a3157
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-03-29 10:48:58 -0600

    Add nickname method

--- lib/player.rb
+++ lib/player.rb
@@ -1,23 +1,30 @@
 class Player
   attr_reader :name,
               :monthly_cost,
-              :contract_length
+              :contract_length,
+              :nickname
 
   def initialize(name, monthly_cost, contract_length)
     @name = name
     @monthly_cost = monthly_cost
     @contract_length = contract_length
+    @nickname = nil
   end
-
+  
   def first_name
     @name.split(/ /, 2)[0] 
   end
-
+  
   def last_name
     @name.split(/ /, 2)[1] 
   end
-
+  
   def total_cost
     monthly_cost * contract_length
   end
+
+  def set_nickname!(nickname)
+    @nickname = nickname
+  end
+
 end
