bb0d6ce4a9edfaae17f0dba9c46015b581b0e460
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-03-29 09:46:45 -0600

    Add first_name method

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,14 @@
 class Player
-  
+
+
   def initialize(name, monthly_cost, contract_length)
     @name = name
     @monthly_cost = monthly_cost
     @contract_length = contract_length
+  end
+
+  def first_name
+    @name.split(/ /, 2)[0] 
+    
   end
 end
